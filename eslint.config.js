08d42f2abfa3acb58bac3d88bfcc9bd664745e04
usman-asim <usmandevlpr@gmail.com>
2026-03-31 16:14:31 +0500

    updated github action ci.yml file

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        module: "readonly",
+        require: "readonly",
+        exports: "readonly",
+        __dirname: "readonly",
+      },
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+];
+
